68d441e6027fdd211750cfe68898a8cdcedac872
lockdef <locktki@icloud.com>
2020-05-29 19:58:13 +0900

    firebaseと環境変数の追加

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_DB,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
