14fb602928609234568a464fb9fd65f89040fa7d
lockdef <locktki@icloud.com>
2020-05-29 19:59:03 +0900

    一問のみCTF問題を表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="home">
     <v-container class="mx-8">
-      <h1>Web</h1>
+      <h1 class="mt-4 display-3">Web</h1>
+      <v-row>
+        <v-col cols="12" sm="6" md="4" lg="3">
+          <v-hover v-slot:default="{ hover }">
+            <a class="d-block">
+              <v-card :elevation="hover ? 12 : 2" color="teal darken-1" class="mt-5" outlined>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="white--text headline text-center">Nihao</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card>
+            </a>
+          </v-hover>
+        </v-col>
+      </v-row>
+      <Signup></Signup>
     </v-container>
   </div>
 </template>
@@ -9,9 +25,12 @@
 <script>
 // @ is an alias to /src
 
+import Signup from '@/components/Signup'
+
 export default {
   name: 'Home',
   components: {
+    Signup
   }
 }
 </script>
