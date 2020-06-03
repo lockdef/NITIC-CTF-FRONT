b9d9007fe3d244c8b711b7718d54e2b3a7aea992
lockdef <locktki@icloud.com>
2020-06-03 19:30:42 +0900

    デバッグ出力を削除

--- src/views/Contest.vue
+++ src/views/Contest.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="contest">
-    <v-container class="mx-8">
+    <v-container>
       <h1 class="mt-4 display-3">Web</h1>
       <v-row>
         <v-col cols="12" sm="6" md="4" lg="3">
-          <v-hover v-slot:default="{ hover }">
-            <a class="d-block">
-              <v-card
-                :elevation="hover ? 12 : 2"
-                color="light-green darken-3"
-                class="mt-5"
-                outlined
-              >
-                <v-list-item>
-                  <v-list-item-content>
-                    <v-list-item-title class="white--text headline text-center">Nihao</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-card>
-            </a>
-          </v-hover>
+          <v-dialog v-model="dialog">
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on" color="light-green darken-3" width="100%">
+                <span class="white--text headline">Nihao</span>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-list-item-content class="mx-6">
+                <v-list-item-title>
+                  <h1 class="mt-2 mb-4">NIHAO</h1>
+                </v-list-item-title>
+                <p>あああああああああああああああああああああああ</p>
+                <v-card-actions>
+                  <v-btn @click="dialog = false">Close</v-btn>
+                </v-card-actions>
+              </v-list-item-content>
+            </v-card>
+          </v-dialog>
         </v-col>
       </v-row>
     </v-container>
@@ -33,6 +35,7 @@ export default {
   name: 'Contest',
   data () {
     return {
+      dialog: false,
       all_problems: [{
         type: 'Web',
         problems: [{
